6e414d27a9cf5c570c2bfbf3916773bc3e276042
Freddy Chiron <freddy.chiron@gmail.com>
2019-07-14 07:43:28 +0200

    ajout d'une conf par fichier properties

--- src/main/java/org/esgi/Pianist.java
+++ src/main/java/org/esgi/Pianist.java
@@ -2,7 +2,10 @@ package org.esgi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class Pianist implements Artist {
--- src/main/java/org/esgi/Piano.java
+++ src/main/java/org/esgi/Piano.java
@@ -1,12 +1,23 @@
 package org.esgi;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+@Configuration
+@PropertySource({
+        "classpath:application.properties"
+})
 @Component("piano")
 public class Piano implements Instrument {
+
+    @Value( "${piano.sound}" )
+    private String sound;
+
     @Override
     public String play() {
-        return " PLINK PLINK PLINK";
+        return sound;
     }
 }
